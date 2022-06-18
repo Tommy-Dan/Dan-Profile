d927f08d6880870733644eb749cb40d387a5979c
Tommy-Dan <ttuckerwra@gmail.com>
2022-06-18 17:52:18 +0000

    added feature

--- js/script.js
+++ js/script.js
@@ -56,3 +56,19 @@ $(document).ready(function(){
         }
     });
 });
+function readmore(){
+    var dots = document.getElementById("dots");
+    var moretext = document.getElementById("more");
+    var btn = document.getElementById("btn");
+
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        btn.innerHTML = "Read more";
+        moretext.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btn.innerHTML = "Read Less";
+        moretext.style.display = "inline";
+    }
+}
